2dc768127413577feadfc7588afe011a1931f290
myranto <95678823+myranto@users.noreply.github.com>
2024-06-06 14:25:39 +0300

    init backend project

---
+++ ws_gondor/src/main/java/gondor/chic/ws_gondor/WsGondorApplication.java
@@ -0,0 +1,13 @@
+package gondor.chic.ws_gondor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsGondorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsGondorApplication.class, args);
+    }
+
+}
---
+++ ws_gondor/src/test/java/gondor/chic/ws_gondor/WsGondorApplicationTests.java
@@ -0,0 +1,13 @@
+package gondor.chic.ws_gondor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsGondorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
