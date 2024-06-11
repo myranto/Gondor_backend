e887bb090674203ca69039af45ddd85057149366
bryancg-7 <hartrand13@gmail.com>
2024-06-11 15:58:07 +0300

    integration

--- script_gimli/data.sql
+++ script_gimli/data.sql
@@ -1,10 +1,10 @@
 INSERT INTO t_produit VALUES
-    ('PRD'||nextval('sProduit'),'REF'||nextval('sProduitReference'),'Baton enchanté de Lumos',TRUE, 500, 10, 'baton-enchante-lumos.jpg'),
-    ('PRD'||nextval('sProduit'),'REF'||nextval('sProduitReference'),'Potion d''éternel jeunesse',TRUE, 500, 8, 'potion-eternel-jeunesse.jpg'),
-    ('PRD'||nextval('sProduit'),'REF'||nextval('sProduitReference'),'Crystak mythique',TRUE, 500, 10, 'crystal-mythique.jpg'),
-    ('PRD'||nextval('sProduit'),'REF'||nextval('sProduitReference'),'Elixir d''invisibilité',TRUE, 200, 5, 'elixir-invisibilite.jpg'),
-    ('PRD'||nextval('sProduit'),'REF'||nextval('sProduitReference'),'Dictionnaire des sociers',TRUE, 400, 2, 'dictionnaire-sorciers.jpg'),
-    ('PRD'||nextval('sProduit'),'REF'||nextval('sProduitReference'),'Amulette de protection',TRUE, 400, 4, 'amulette-protections.jpg'),
-    ('PRD'||nextval('sProduit'),'REF'||nextval('sProduitReference'),'Balai de sorcier',TRUE, 250, 2, 'balai-sorcier.jpg'),
-    ('PRD'||nextval('sProduit'),'REF'||nextval('sProduitReference'),'Plume du phoenix',TRUE, 150, 5, 'plume-phoenix.jpg')
+    ('PRD'||nextval('sProduit'),'REF'||nextval('sProduitReference'),'Baton enchanté de Lumos',TRUE, 500, 10, 'assets/products/Enchanted-wand-of-lumos.webp'),
+    ('PRD'||nextval('sProduit'),'REF'||nextval('sProduitReference'),'Potion d''éternel jeunesse',TRUE, 500, 8, 'assets/products/Potion-of-eternal-youth.webp'),
+    ('PRD'||nextval('sProduit'),'REF'||nextval('sProduitReference'),'Crystak mythique',TRUE, 500, 10, 'assets/products/Mystic-crystal-orb.webp'),
+    ('PRD'||nextval('sProduit'),'REF'||nextval('sProduitReference'),'Elixir d''invisibilité',TRUE, 200, 5, 'assets/products/Elixir-of-invisibility.webp'),
+    ('PRD'||nextval('sProduit'),'REF'||nextval('sProduitReference'),'Dictionnaire des sociers',TRUE, 400, 2, 'assets/products/Sorcerers-spellbook.webp'),
+    ('PRD'||nextval('sProduit'),'REF'||nextval('sProduitReference'),'Amulette de protection',TRUE, 400, 4, 'assets/products/Amulet-of-protection.webp'),
+    ('PRD'||nextval('sProduit'),'REF'||nextval('sProduitReference'),'Balai de sorcier',TRUE, 250, 2, 'assets/products/Witchs-broomstick.webp'),
+    ('PRD'||nextval('sProduit'),'REF'||nextval('sProduitReference'),'Plume du phoenix',TRUE, 150, 5, 'assets/products/Phoenix-feather-quill.webp')
 ;
--- ws_gondor/src/main/java/gondor/chic/ws_gondor/controleur/ProduitControleur.java
+++ ws_gondor/src/main/java/gondor/chic/ws_gondor/controleur/ProduitControleur.java
@@ -1,19 +1,19 @@
 package gondor.chic.ws_gondor.controleur;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import gondor.chic.ws_gondor.metierServices.ProduitManager;
 
 // @RequestMapping(path = "api/v1/produits")
 @RestController
 @RequestMapping(path = "/")
+
+@CrossOrigin(origins = "*")
 public class ProduitControleur {
     @SuppressWarnings("unused")
     private final ProduitManager produitService;
