6720ec73da1c7e91a98b5ec5216d75828913fe02
myranto <95678823+myranto@users.noreply.github.com>
2024-06-17 13:30:35 +0300

    Update ProduitRepository.java

--- ws_gondor/src/main/java/gondor/chic/ws_gondor/repository/ProduitRepository.java
+++ ws_gondor/src/main/java/gondor/chic/ws_gondor/repository/ProduitRepository.java
@@ -2,25 +2,14 @@ package gondor.chic.ws_gondor.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import gondor.chic.ws_gondor.metier.metierRelationel.Produit;
 
-public interface ProduitRepository extends JpaRepository<Produit, String>, 
+public interface ProduitRepository extends JpaRepository<Produit, String>,
         JpaSpecificationExecutor<Produit> {
 
     @Query(value = "select * from t_produit where estdujour = true LIMIT 1", nativeQuery = true)
     Produit rechercherProduitDuJour();
-
-    @Transactional
-    @Modifying(clearAutomatically = true)
-    @Query(value = "update t_produit set estDuJour=false where estDuJour=true", nativeQuery = true)
-    void removeProduitDuJour();
-
-    @Transactional
-    @Modifying(clearAutomatically = true)
-    @Query(value = "update t_produit set estDuJour=true where reference=?1", nativeQuery = true)
-    void setProduitDuJour(String reference); 
 
 }
